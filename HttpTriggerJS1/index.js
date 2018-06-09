190632331b85e0dbad368b68f9df1648bea5771a
Rehan <rehans2k3@gmail.com>
2018-06-09 14:39:01 +0500

    initial commit

--- HttpTriggerJS1/index.js
+++ HttpTriggerJS1/index.js
@@ -4,13 +4,13 @@ module.exports = function (context, req) {
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "hi " + (req.query.name || req.body.name)
+            body: "HI " + (req.query.name || req.body.name)
         };
     }
     else {
         context.res = {
             status: 400,
-            body: "Please pass a name on the query string or in the request body"
+            body: "Please pass a name on the query string or in the request body!!!"
         };
     }
     context.done();
